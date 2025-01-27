226e4042d30ee0c8788100f645c664b60b0df07e
jvdevs <joaodarklegal@gmail.com>
2025-01-27 16:30:18 -0300

    Converta graus celsius em graus farenheirt

---
+++ ex13.py
@@ -0,0 +1,3 @@
+celsius = float(input('digite a temeperatura atual em graus celsius: '))
+f =  ((9 * celsius)/ 5) +32
+print('a temperatura informada {} convertida em graus farenheirt fica igual a: {}'.format(celsius, f))
