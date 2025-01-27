a8d9bfbec74782c364386d3cf093af755c90654b
jvdevs <joaodarklegal@gmail.com>
2025-01-27 14:23:14 -0300

    Leia um numero e diga seu dobro, triplo e sua raiz quadrada

---
+++ ex5.py
@@ -0,0 +1,5 @@
+n1 = int(input('digite um número: '))
+dobro = n1 * 2
+triplo = n1 * 3
+raiz_quadrada = n1 ** (1/2)
+print("o dobro de {} vale {}, o triplo vale {}, e a raiz quadrada vale {}".format(n1 , dobro, triplo, raiz_quadrada))
