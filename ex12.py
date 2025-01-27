e78d587daf42dc590c3e2ad78711db9ede34126d
jvdevs <joaodarklegal@gmail.com>
2025-01-27 16:26:54 -0300

    Leia o salário de um funcionário e mostre o novo salário com um aumento de 15%

---
+++ ex12.py
@@ -0,0 +1,3 @@
+salario = float(input('Qual o seu salário atual: '))
+novo_salario = salario + ( salario * 15 / 100)
+print('o seu salario de {} juntamente com o aumento de 15%, chegou a um valor de {}'.format(salario , novo_salario))
