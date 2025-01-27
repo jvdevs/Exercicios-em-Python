9b9cc2ac87f273e0e13c6a9908cd3dd9de0b8dbf
jvdevs <joaodarklegal@gmail.com>
2025-01-27 16:19:10 -0300

    Leia a largura e altura de uma parede e diga sua area e a quantidade de tinta necessaria para pinta-la

---
+++ ex10.py
@@ -0,0 +1,6 @@
+largura = float(input("digite em metros, a largura: "))
+altura = float(input('digite em metros, a altura da parede: '))
+area = largura * altura
+print('a area da parede é de:  {} metros quadrados'.format(area))
+tinta = area / 2
+print('será necessário de: {}L de tinta'.format(tinta))
