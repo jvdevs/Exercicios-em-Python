97378e07560e6a28c9485e1fe83acb0a86f97a48
jvdevs <joaodarklegal@gmail.com>
2025-01-27 14:35:57 -0300

    Leia um valor em metros e exiba o mesmo em centrimetros e milimetros

---
+++ ex7.py
@@ -0,0 +1,4 @@
+valor = float(input('digite um número em metros: '))
+cm = valor * 100
+mili = valor * 1000
+print('Considerando os metros {} ,em centimetros ficam {} e em milimetros {}'.format(valor, cm, mili))
