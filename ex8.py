cd7ea5cd33a17168c45985d866159d06b30258a7
jvdevs <joaodarklegal@gmail.com>
2025-01-27 14:42:49 -0300

    Leia um número e diga sua tabuada

---
+++ ex8.py
@@ -0,0 +1,13 @@
+n1 = int(input("digite um numero: "))
+tab_1 = n1 *1
+tab_2 = n1 *2
+tab_3 = n1 *3
+tab_4 = n1 *4
+tab_5 = n1 *5
+tab_6 = n1 *6
+tab_7 = n1 *7
+tab_8 = n1 *8
+tab_9 = n1 *9
+tab_10 = n1 *10
+print("Segue a tabuada do {}".format(n1))
+print('{},{},{},{},{},{},{},{},{},{}'.format(tab_1, tab_2, tab_3, tab_4, tab_5, tab_6, tab_7, tab_8, tab_9, tab_10))
