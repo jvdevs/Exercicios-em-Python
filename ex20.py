b5bf46410b62f3bb7c6e05dec71a4d676b9f2d2e
jvdevs <joaodarklegal@gmail.com>
2025-01-28 17:06:12 -0300

    Leia um nome completa e peça como é escrito em minusculo, maiusculo , quantas letras tem e quantas letras tem somente o primeiro nome

---
+++ ex20.py
@@ -0,0 +1,6 @@
+nome = str(input('Digite seu noke completo: ')).strip()
+print('Seu nome em maiusculo é: {}'.format(nome.upper()))
+print('Seu nome em minusculo é: {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras'.format(separa[0], len(separa[0])))
