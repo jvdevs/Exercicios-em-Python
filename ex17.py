af8b714c6a7ae8d70299d5e610bfa91a80895a0e
jvdevs <joaodarklegal@gmail.com>
2025-01-28 16:43:31 -0300

    Leia um angulo e diga seu seno, cosseno e tangente

---
+++ ex17.py
@@ -0,0 +1,10 @@
+import math
+
+angulo = float(input("Digite a medida do angulo: "))
+sen = math.sin(math.radians)(angulo)
+cos = math.cos(math.radians)(angulo)
+tan = math.tan(math.radians)(angulo)
+print("a medida do seno é de: {}".format(sen))
+print("a medida do cosseno é de: {}".format(cos))
+print("a medida da tangente é de: {}".format(tan))
+
